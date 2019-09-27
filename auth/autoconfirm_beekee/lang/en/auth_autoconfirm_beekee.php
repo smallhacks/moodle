f9406cd4e1d4526466511a61495c8c4b647e8cba
Vincent Widmer <eodecorp@hotmail.com>
2019-09-27 15:58:58 +0200

    Add custom AutoConfirm plugin

---
+++ auth/autoconfirm_beekee/lang/en/auth_autoconfirm_beekee.php
@@ -0,0 +1,7 @@
+<?php
+$string['auth_autoconfirm_beekeedescription'] = 'Requires email address but no email confirmation.';
+$string['auth_autoconfirm_beekeerecaptcha'] = 'Adds a visual/audio confirmation form element to the signup page for email self-registering users. This protects your site against spammers and contributes to a worthwhile cause. See http://www.google.com/recaptcha/learnmore for more details. <br /><em>PHP cURL extension is required.</em>';
+$string['auth_autoconfirm_beekeerecaptcha_key'] = 'Enable reCAPTCHA element';
+$string['auth_autoconfirm_beekeesettings'] = 'Settings';
+$string['pluginname'] = 'Auto Confirm';
+$string['autoconfirm_beekee'] = '<p>Please click on the link below to confirm your new account.</p>';
